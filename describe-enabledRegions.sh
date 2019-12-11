64cf6c3737c453a7d7e9bf1989587ce0d073c870
xonor <xonoroneill@gmail.com>
2019-12-11 09:29:21 +0000

    Update describe-enabledRegions.sh

--- describe-enabledRegions.sh
+++ describe-enabledRegions.sh
@@ -1,3 +1,3 @@
-Here is a Bash one-liner using the AWS CLI that I put together to list enabled regions. It's not ideal but it works.
+Here is a Bash one-liner using the AWS CLI that I put together to list enabled regions.
 
 $ for i in $(aws ec2 describe-regions --output text --query "Regions[].RegionName"); do aws ec2 describe-availability-zones --query "AvailabilityZones[0].RegionName" --region $i --output text; done 2>/dev/null
