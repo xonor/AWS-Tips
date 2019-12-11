e0ad396e88bf224a079392631a90d479767a90d7
xonor <xonoroneill@gmail.com>
2019-12-11 12:00:32 +0000

    Update describe-enabledRegions.sh

--- describe-enabledRegions.sh
+++ describe-enabledRegions.sh
@@ -1,3 +1,6 @@
 Here is a Bash one-liner using the AWS CLI that I put together to list enabled regions.
 
+This became useful when AWS released the Hong Kong region without enabling it by defualt on all accounts.
+The problem was that when automation scripts covering all regions would do describes, Hong Kong would fail.
+
 $ for i in $(aws ec2 describe-regions --output text --query "Regions[].RegionName"); do aws ec2 describe-availability-zones --query "AvailabilityZones[0].RegionName" --region $i --output text; done 2>/dev/null
